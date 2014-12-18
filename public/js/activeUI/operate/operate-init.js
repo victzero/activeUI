b9f5fa35c9b5063aa1918c59f384442ebe770b4c
zero-nci <victzero@gmail.com>
2014-12-18 14:11:35 +0800

    保存为图片

--- public/js/activeUI/operate/operate-init.js
+++ public/js/activeUI/operate/operate-init.js
@@ -21,7 +21,22 @@ var toolbarCtrl = ['$scope', function($scope) {
   configToolbar($scope);
 }];
 var configToolbar = function($scope) {
-  $scope.tools = actGlobal.tools;
+  $scope.tools = [{
+    title: '移动|选择',
+    active: 'active',
+    mode: 'move'
+  }, {
+    title: '连线|拖动',
+    active: '',
+    mode: 'connect'
+  }, {
+    title: '保存图片',
+    active: '',
+    mode: null, //不影响模式
+    action: function() { //不影响模式,但是有直接操作.
+      saveImage2Local(false);
+    }
+  }];
 
   $scope.chooseTool = function(index) {
     var tool = $scope.tools[index];
@@ -35,10 +50,56 @@ var configToolbar = function($scope) {
         }
       }
     }
-    if(tool.action){
+    if (tool.action) {
       tool.action();
     }
   }
 };
 
 app.controller('toolbarCtrl', toolbarCtrl);
+
+/**
+ * [saveImage2Local description]
+ * 保存图片到本地.
+ * @param {boolean} save true为保存到本地,false为新窗口中打开
+ * @return {[type]} [description]
+ */
+function saveImage2Local(save) {
+  // 图片导出为 png 格式
+  var type = 'png';
+  var imgData = canvas.toDataURL(type);
+  if (!save) {
+    window.open(imgData)
+    return;
+  }
+  // 加工image data，替换mime type
+  imgData = imgData.replace(_fixType(type), 'image/octet-stream');
+  // 下载后的问题名
+  var filename = 'activeUI_' + (new Date()).getTime() + '.' + type;
+  // download
+  saveFile(imgData, filename);
+};
+/**
+ * 获取mimeType
+ * @param  {String} type the old mime-type
+ * @return the new mime-type
+ */
+var _fixType = function(type) {
+  type = type.toLowerCase().replace(/jpg/i, 'jpeg');
+  var r = type.match(/png|jpeg|bmp|gif/)[0];
+  return 'image/' + r;
+};
+/**
+ * 在本地进行文件保存
+ * @param  {String} data     要保存到本地的图片数据
+ * @param  {String} filename 文件名
+ */
+var saveFile = function(data, filename) {
+  var save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+  save_link.href = data;
+  save_link.download = filename;
+
+  var event = document.createEvent('MouseEvents');
+  event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  save_link.dispatchEvent(event);
+};
